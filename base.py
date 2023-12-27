dc02ab1f26ec89d5cb6cb9551f2b1e2912063c66
HasibBinSayed <hasibbinsayed11@gmail.com>
2023-12-27 15:38:09 +0600

    delete mp3 and use wav

--- base.py
+++ base.py
@@ -3,9 +3,9 @@ import winsound
 
 def play_sound():
     # Play a sound (you can change the filename or path to your desired sound file)
-    winsound.PlaySound("sound.wav", winsound.SND_FILENAME)
+    winsound.PlaySound("look_around.mp3", winsound.SND_FILENAME)
 
-# Set the time interval in seconds (20 minutes = 20 * 60 seconds)
+# Set the time interval in seconds (20 minutes = 20 * 60 seco
 interval = 20 * 60
 
 while True:
